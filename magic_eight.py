49a99733b03f5a579281be0ca2c6055d49447b59
srelkins <35789181+srelkins@users.noreply.github.com>
2018-01-31 10:57:48 -0500

    fixed errors

--- magic_eight.py
+++ magic_eight.py
@@ -1,20 +1,16 @@
 import random
 def ask_question():
     question = input("Enter your question: ")
+    answers = ["it is certain", "As I see it, yes","It is decidedly so","Without a doubt"," Yes definitely","You may rely on it",
+    "Most likely","Outlook good","Yes","Signs point to yes","Reply hazy try again","Ask again later","Better not tell you now",
+    "Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no", "My sources say no"," Outlook not so good","Very doubtful"]
     while question != "Quit":
+        if question[-1] != "?":
+            print("I'm sorry, I can only answer questions.")
+        else:
+            print(random.choice(answers))
         return question
-    if question == "Quit":
-        break
+    # if question == "Quit":
+    #     break
 
-if question.split([-1]) != "?":
-    print("I’m sorry, I can only answer questions.")
-
-
-def pick_answer():
-	answers = ["it is certain", "As I see it, yes","It is decidedly so","Without a doubt"," Yes definitely","You may rely on it",
-	"Most likely","Outlook good","Yes","Signs point to yes","Reply hazy try again","Ask again later","Better not tell you now",
-	"Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no", "My sources say no"," Outlook not so good","Very doubtful"]
-
-	print(random.choice(answers))
 ask_question()
-pick_answer()
