f127c4890bee8f38b4215f988a8ff3a42cc64681
srelkins <35789181+srelkins@users.noreply.github.com>
2018-01-31 10:31:40 -0500

    Added new changes

--- magic_eight.py
+++ magic_eight.py
@@ -1,5 +1,11 @@
 def ask_question():
     question = input("Enter your question: ")
-    return question
+    while question != "Quit":
+        return question
+    if question == "Quit":
+        break
+
+if question.split([-1]) != "?":
+    print("I’m sorry, I can only answer questions.")
 
 ask_question()
